dce389a63a58e3f47f20243c1904f3ddcda8a3cd
BMikelFrost <101161806+BMikelFrost@users.noreply.github.com>
2022-05-12 22:42:49 -0700

    Add files via upload

---
+++ pokemonaxios/App.js
@@ -0,0 +1,14 @@
+import './App.css'
+import PokemonForm from './components/PokemonForm'
+
+function App() {
+  return (
+    <div className="App">
+      < PokemonForm />
+    </div>
+  )
+}
+
+export default App;
+
+
